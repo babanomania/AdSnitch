ae515a5232c64562742ee74682d09ef6cbe45cbc
Shouvik Basu <babanomania@gmail.com>
2025-06-29 01:00:19 +1000

    Add lockfile

---
+++ src/plugins/mock.js
@@ -0,0 +1,13 @@
+const fs = require('fs').promises;
+
+async function getStats() {
+  const data = JSON.parse(
+    await fs.readFile(require('path').join(__dirname, '..', '..', 'data', 'mock_logs.json'), 'utf8')
+  );
+  // Sort by count descending
+  const sorted = data.sort((a, b) => b.count - a.count);
+  const topDomains = sorted.map((d) => d.domain);
+  return { topDomains };
+}
+
+module.exports = { getStats };
